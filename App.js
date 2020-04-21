20978801bf9ff54793920df23547cee8f2aa222c
dmchiodini <g25_diego@hotmail.com>
2020-04-21 15:48:05 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  TextInput
+} from 'react-native';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    console.log("Será exibido uma vez");
+    this.state = {cronometro: 10, caracteres: '', habilitar: true, array: []};
+    this.inscricaoCronometro;
+  }
+
+componentDidMount() {
+  this.inscricaoCronometro = setInterval(() => {
+    if(this.state.cronometro > 0){
+      this.incrementarCronometro();
+    }
+    if(this.state.cronometro == 0){
+      this.setState({ habilitar: false})
+      this.setState({ array: [...this.state.caracteres] })
+    }
+  }, 1000);
+}
+
+incrementarCronometro() {
+  this.setState(state => ({cronometro: state.cronometro - 1}));
+}
+
+render() {
+  return(
+    <SafeAreaView style={{ padding: 50 }}>
+      <Text>Cronometro: {this.state.cronometro} segundos. </Text>
+      <TextInput 
+        style={{ height: 40, marginTop: 10, marginBottom: 10, backgroundColor: "#CCCCCC" }} 
+        onChangeText={text => this.setState({ caracteres: text.trim()})} 
+        editable={this.state.habilitar?true : false} 
+      />
+      {this.state.habilitar == false && 
+        <Text>Você digitou {this.state.array.length} caracteres em 10 segundos</Text>
+      }   
+    </SafeAreaView>
+  )
+}
+
+}
+
+export default App;
